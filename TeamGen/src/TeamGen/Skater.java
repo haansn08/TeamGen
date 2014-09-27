655dfa807db3a51d7c441f14ec939f35236caa2a
Stefan Haan <stefanhaan08@gmail.com>
2014-09-27 18:32:03 +0200

    Added gender and nation

--- TeamGen/src/TeamGen/Skater.java
+++ TeamGen/src/TeamGen/Skater.java
@@ -4,12 +4,25 @@ package TeamGen;
  * Created by Stefan Haan on 9/27/14.
  */
 public class Skater {
+    public static final int FEMALE = 1;
+    public static final int MALE = 2;
+
     private double time;
-    public Skater(double timeSeconds) {
+    private int gender;
+    private String nation;
+    public Skater(double timeSeconds, int gender, String nation) {
         time = timeSeconds;
+        this.gender = gender;
+        this.nation = nation.toUpperCase();
     }
 
     public double getTimeSeconds() {
         return time;
+    }
+    public int getGender() {
+        return gender;
+    }
+    public String getNation() {
+        return nation;
     }
 }
--- TeamGen/src/tests/SkaterTests.java
+++ TeamGen/src/tests/SkaterTests.java
@@ -8,7 +8,10 @@ import junit.framework.TestCase;
 
 public class SkaterTests extends TestCase{
     public void testConstructor(){
-        Skater skater = new Skater(39.21);
+        Skater skater = new Skater(39.21, Skater.MALE, "aut");
         assertEquals(39.21, skater.getTimeSeconds(), 0.0001);
+        assertEquals(Skater.MALE, skater.getGender());
+        assertEquals("AUT", skater.getNation());
     }
+
 }
