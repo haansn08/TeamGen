73399096615c0fb232ea6eee87763a72f3d9297b
Stefan Haan <stefanhaan08@gmail.com>
2014-09-30 13:55:25 +0200

    Refactoring

--- TeamGen/src/TeamGen/OptimalTeamGenerator.java
+++ TeamGen/src/TeamGen/OptimalTeamGenerator.java
@@ -27,40 +27,46 @@ public class OptimalTeamGenerator extends TeamGenerator { //The name is a joke
         Draw result = new Draw();
         List<Team> possibleTeams = getPossibleTeams();
         while (possibleTeams.size() > 0){
-            //find best team
-            double bestTeamAverageDiff = Double.MAX_VALUE;
-            Team bestTeam = null;
-            for (Team team : possibleTeams) {
-                double teamAverageDiff = Math.abs(team.totalTime() - optimalTeamAverage);
-                if (teamAverageDiff < bestTeamAverageDiff){
-                    bestTeamAverageDiff = teamAverageDiff;
-                    bestTeam = team;
-                }
-            }
+            Team bestTeam = findBestTeam(possibleTeams);
             result.addTeam(bestTeam);
-            //remove used skaters
-            for (Skater skater : bestTeam)
-                skaterPool.remove(skater);
-            possibleTeams = getPossibleTeams();
+            removeUsedSkaters(bestTeam);
+
+            possibleTeams = getPossibleTeams(); //re-calculate possible teams
         }
         return result;
     }
 
     private List<Team> getPossibleTeams() throws Exception {
         List<Team> possibleTeams = new ArrayList<Team>();
-        for (int s1 = 0; s1 < skaterPool.size(); s1++) {
-            for (int s2 = s1 + 1; s2 < skaterPool.size(); s2++) {
-                for (int s3 = s2 + 1; s3 < skaterPool.size(); s3++) {
-                    try{
+        for (int s1 = 0; s1 < skaterPool.size(); s1++)
+            for (int s2 = s1 + 1; s2 < skaterPool.size(); s2++)
+                for (int s3 = s2 + 1; s3 < skaterPool.size(); s3++)
+                    try {
                         Team team = new Team();
                         team.addSkater(skaterPool.get(s1));
                         team.addSkater(skaterPool.get(s2));
                         team.addSkater(skaterPool.get(s3));
                         possibleTeams.add(team);
-                    } catch (InvalidSkaterException ignored) {}
-                }
+                    }
+                    catch (InvalidSkaterException ignored) {}
+        return possibleTeams;
+    }
+
+    private void removeUsedSkaters(Team bestTeam) {
+        for (Skater skater : bestTeam)
+            skaterPool.remove(skater);
+    }
+
+    private Team findBestTeam(List<Team> possibleTeams) {
+        double bestTeamAverageDiff = Double.MAX_VALUE;
+        Team bestTeam = null;
+        for (Team team : possibleTeams) {
+            double teamAverageDiff = Math.abs(team.totalTime() - optimalTeamAverage);
+            if (teamAverageDiff < bestTeamAverageDiff){
+                bestTeamAverageDiff = teamAverageDiff;
+                bestTeam = team;
             }
         }
-        return possibleTeams;
+        return bestTeam;
     }
 }
